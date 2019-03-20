13a35d9b1cd605b426fae0045eaceb5fcf671d56
Helen Liutongco <liutongcoh@gmail.com>
2019-03-20 11:13:19 -0400

    Push code

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.htm'))
-})
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/index.htm'))
+// })
 
-app.get('/video', function(req, res) {
-  const path = 'assets/sample.mp4'
+function helper(req, res, path){
   const stat = fs.statSync(path)
   const fileSize = stat.size
   const range = req.headers.range
@@ -41,6 +40,26 @@ app.get('/video', function(req, res) {
     res.writeHead(200, head)
     fs.createReadStream(path).pipe(res)
   }
+}
+
+app.get('/clip1', function(req, res) {
+  const path = 'assets/crying-min.mp4'
+  helper(req, res, path)
+})
+
+app.get('/clip2', function(req, res) {
+  const path = 'assets/roads-min.mp4'
+  helper(req, res, path)
+})
+
+app.get('/clip3', function(req, res) {
+  const path = 'assets/the-truth-min.mp4'
+  helper(req, res, path)
+})
+
+app.get('/clip4', function(req, res) {
+  const path = 'assets/yoda-min.mp4'
+  helper(req, res, path)
 })
 
 app.listen(3000, function () {
